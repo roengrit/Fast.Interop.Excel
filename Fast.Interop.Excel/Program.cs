3cff575ece05a9c83645b7167052c20f125ca839
logon.firstclass@gmail.com <logon.firstclass@gmail.com>
2017-07-11 11:48:33 +0700

    Add project files.

---
+++ Fast.Interop.Excel/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fast.Interop.Excel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
