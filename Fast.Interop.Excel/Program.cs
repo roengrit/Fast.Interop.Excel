6230206dc328904fd9055d7648c60373623ff972
logon.firstclass@gmail.com <logon.firstclass@gmail.com>
2017-07-13 10:02:02 +0700

    complete

--- Fast.Interop.Excel/Program.cs
+++ Fast.Interop.Excel/Program.cs
@@ -1,8 +1,11 @@
-﻿using System;
+﻿using Fast.Interop.Excel;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ExcelX = Microsoft.Office.Interop.Excel;
 
 namespace Fast.Interop.Excel
 {
@@ -10,6 +13,35 @@ namespace Fast.Interop.Excel
     {
         static void Main(string[] args)
         {
+            var SampleData = new SampleData().GenData(); //สร้างข้อมูลทดสอบ
+            object[,] RawData = new object[SampleData.Rows.Count, SampleData.Columns.Count];
+
+            //เก็บข้อมูลใน Array
+            for (int i = 0; i <= SampleData.Rows.Count - 1; i++)
+            {
+                for (int j = 0; j <= SampleData.Columns.Count - 1; j++)
+                {
+                    RawData[i, j] = SampleData.Rows[i].ItemArray[j].ToString();
+                }
+            }
+
+            /// ใส่ข้อมูล และ เปิด Excel 
+            ExcelX.Application App;
+            ExcelX.Workbook WorkBook;
+            ExcelX.Worksheet WorkSheet;
+            object MisValue = System.Reflection.Missing.Value;
+            App = new ExcelX.Application();
+            WorkBook = App.Workbooks.Add(MisValue);
+            WorkSheet = (ExcelX.Worksheet)WorkBook.Worksheets.get_Item(1);      
+                   
+            //จุดสำคัญ
+            var StartCell = WorkSheet.Cells[1, 1];
+            var EndCell = WorkSheet.Cells[SampleData.Rows.Count, SampleData.Columns.Count];
+            var WriteRange = WorkSheet.Range[StartCell, EndCell];
+            WriteRange.Value2 = RawData; 
+
+            App.Visible = true;
+        
         }
     }
 }
---
+++ Fast.Interop.Excel/SampleData.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fast.Interop.Excel
+{
+    class SampleData
+    {
+        public DataTable GenData()
+        {
+            var Data = new DataTable();
+
+            Data.Columns.Add("Running", typeof(int));
+            Data.Columns.Add("TextVal", typeof(string));
+
+            for (int i = 0; i < 1000000; i++)
+            {
+                var NewRow = Data.NewRow();
+                NewRow["Running"] = i;
+                NewRow["TextVal"] = Guid.NewGuid().ToString();
+                Data.Rows.Add(NewRow);
+            }
+            return Data;
+        }
+    }
+}
